eddefce8d250ff921aec30e791f8e370b411fbbe
pascallin <pascal_lin@foxmail.com>
2022-09-24 11:53:17 +0800

    chore: remove useless import

--- service/user.go
+++ service/user.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
-	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
